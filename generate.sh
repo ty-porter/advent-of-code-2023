53f1690b522fcee4d07bdbe3bc6aef4e3a682414
Tyler Porter <tyler.b.porter@gmail.com>
2023-12-24 17:57:48 -0500

    Fix directory structure and generator

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu pipefail
 
-PREVDAY=$(ls | grep day | sort -V | tail -n 1 | tr -d '\n' | tail -c 1)
+PREVDAY=$(ls | grep day | sort -V | tail -n 1 | tr -d '\n' | tail -c 2)
 NEXTDAY=$((PREVDAY + 1))
 NEXTDAY_DIR="day$NEXTDAY"
 
@@ -15,7 +15,7 @@ pushd $NEXTDAY_DIR
 cp ../main.go.template main.go
 
 # Copy the day into the go template input path lookup
-sed -i "s/<#>/$NEXTDAY/g" main.go
+sed -i '' "s/<#>/$NEXTDAY/g" main.go
 
 # Init the mod
 go mod init
